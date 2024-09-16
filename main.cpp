b8c0eb381ee1c5619abc6c787a693babad1c8460
Максим Л. Гойдин <MLGoydin@stud.kantiana.ru>
2024-09-16 15:36:23 +0200

    pre_l1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
-int main()
-{
-	std::cout << "Hello, 1 FIIT and 1 MO!" << std::endl;
-	return 0;
+using namespace std;
+
+int main() {
+int a {109};
+int b {17};
+cout << a % b << endl;  // 7
+
+short c {-23};
+short d {8};
+cout << c * d << endl;  // -184
+
+long e {21474};
+long f {33333};
+cout << e - f << endl;  // -11859
+
+float g {3.121};
+float j {1.66};
+cout << g - j << endl;  // 1.461
+
+char x {'M'};
+char y {'F'};
+cout << x + y << endl;  // 147
+
+bool l {true};
+bool k {false};
+
+cout << l - k << endl;  // 147
 }
+
+
