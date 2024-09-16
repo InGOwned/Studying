a6f895bd5b08de98c6ae03536ff2bf2cd63e208e
Максим Л. Гойдин <MLGoydin@stud.kantiana.ru>
2024-09-16 15:53:48 +0200

    update l1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ int a {109};
 int b {17};
 cout << a % b << endl;  // 7
 
+unsigned int v {109};
+unsigned int n {17};
+cout << v + n << endl;  // 6
+
 short c {-23};
 short d {8};
 cout << c * d << endl;  // -184
