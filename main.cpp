0ccc5d618ee325dc52001f23d815ad8f58a157cf
InGOwned <maksim.gojdin@gmail.com>
2024-09-17 23:03:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,34 +29,43 @@ cout << "Data Type  : float\n"
 	<< "----------------------------\n";
 
 cout << "OPERATIONS" << endl;
-int a {109};
-int b {17};
-cout << a % b << endl;  // 7
+#include <iostream>
 
-unsigned int v {109};
-unsigned int n {17};
-cout << v + n << endl;  // 126
+using namespace std;
 
-short c {-23};
-short d {8};
-cout << c * d << endl;  // -184
+int a {120}; 
+int b {23};
+cout << "a = " << a << ", b = " << b << ", a % b = " << a % b << endl;  // 12
 
-long e {21474};
-long f {33333};
-cout << e - f << endl;  // -11859
+unsigned int v {150};
+unsigned int n {75};
+cout << "v = " << v << ", n = " << n << ", v + n = " << v + n << endl;  // 225
 
-float g {3.121};
-float j {1.66};
-cout << g - j << endl;  // 1.461
+short c {7};
+short d {-5};
+cout << "c = " << c << ", d = " << d << ", c * d = " << c * d << endl;  // -35
 
-char x {'M'};
-char y {'F'};
-cout << x + y << endl;  // 147
+long e {80000};
+long f {35000};
+cout << "e = " << e << ", f = " << f << ", e - f = " << e - f << endl;  // 45000
+
+float g {9.85};
+float j {4.67};
+cout << "g = " << g << ", j = " << j << ", g / j = " << g / j << endl;  // 2.109
+
+char x {'Z'};
+char y {'X'};
+cout << "x = "<< x << ", y = " << y << ", x - y = " << x - y << endl;  // 2
 
 bool l {true};
 bool k {false};
+cout << "l = " << l << ", k = " << k << ", l && k = " << (l && k) << endl;  // 0
 
-cout << l - k << endl;  // 147
+double m {5.432};
+double q {2.876};
+cout << "m = " << m << ", q = " << q << ", m * q = " << m * q << endl;  // 15.679
+
+unsigned long r {200000};
+unsigned long s {80000};
+cout << "r = " << r << ", s = " << s << ", r / s = " << r / s << endl;  // 2
 }
-
-
