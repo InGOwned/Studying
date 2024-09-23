45117c5db33cdcc410f43cafec20810564401d23
Maxim_Goydin_University <MLGoydin@stud.kantiana.ru>
2024-09-23 15:40:35 +0200

    Solving lab

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
-int main()
-{
-	std::cout << "Hello, 1 FIIT and 1 MO!" << std::endl;
-	return 0;
+using namespace std;
+
+int main() {
+    int x = 0b101001010111;
+    int bit_value = 0;
+    int position;
+
+	cout << "Enter position: ";
+	cin >> position;
+
+    cout << "\nx = " << x << endl;
+
+    if ((x & (1u << position)) != 0) {
+        bit_value = 1;
+    } else {
+        bit_value = 0;
+    }
+
+    cout << "\nValue of the " << position << "-th bit is: " << bit_value << endl;
+
+    return 0;
 }
