68293d433d9d9a0ce36bbff2fc820cc196f979f3
InGOwned <maksim.gojdin@gmail.com>
2024-11-10 19:15:43 +0200

    Solve

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <climits> 
+#include <iomanip>
 
 using namespace std;
 
+bool positiveAndContainsNumbersThree(int x);
+bool allDiagonals(int A[100][100], int n, int m);
+
+int n;
+
+
 int main()
 {
-	int n, m;
     cout << "Enter the dimension of matrix, n: ";
     cin >> n;
 
@@ -17,5 +24,105 @@ int main()
             cin >> A[i][j];
         }
     }
+
+	if (allDiagonals(A, n, n)) 
+	{	
+		
+		for (int j = 0; j < n; j++)
+		{
+			int min = INT_MAX;
+			int index;
+			int sum_squares = 0;
+
+			for (int i = 0; i < n; i++)
+			{
+				if (A[i][j] < min)
+				{
+					min = A[i][j];
+					index = i;
+				}
+				sum_squares += A[i][j] * A[i][j];
+			}
+
+			A[index][j] = sum_squares;
+		}
+
+		cout << "Modifed matrix: " << endl;
+	
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				cout << setw(6) << A[i][j];
+			}
+			cout << endl;
+		}
+
+	} else {
+		cout << "Matrix hasn't changed: " << endl;
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				cout << setw(6) << A[i][j];
+			}
+			cout << endl;
+		}
+	}
+
+	
+
 	return 0;
+}
+
+
+bool positiveAndContainsNumbersThree(int x) 
+{
+	if (x > 0)
+	{
+		int tmp = x;
+
+		while (tmp) 
+		{
+			if ((tmp % 10 == 3) || (tmp % 10 == 5)) 
+			{
+				return true;
+			} else {
+				tmp /= 10;
+			}
+		}
+	}
+
+	return false;
+}
+
+
+bool allDiagonals(int A[100][100], int n, int m) 
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < n && j < n)
+	{
+		if (!positiveAndContainsNumbersThree(A[i][j])) 
+		{
+			return false;
+		}
+		i++;
+		j++;
+	}
+
+	i = n - 1;
+
+	while (i >= 0) 
+	{
+		for (j = 0; j < n; j++) 
+		{
+			if (!positiveAndContainsNumbersThree(A[i][j])) 
+			{
+				return false;
+			}
+			i--;
+		}
+		
+	}
+
+	return true;
 }
