d878ac398ca3eb79aa40dc7d4107281d7c5d2884
InGOwned <maksim.gojdin@gmail.com>
2024-11-10 16:42:30 +0200

    Test

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello, 1 FIIT and 1 MO!" << endl;
+	int n, m;
+    cout << "Enter the dimension of matrix, n: ";
+    cin >> n;
+
+    int A[100][100];
+
+  
+    cout << "Enter the elements of the matrix: " << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> A[i][j];
+        }
+    }
 	return 0;
 }
