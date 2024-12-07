6799d9a78d5dcdd7fb4024276663e1f49cde76ad
InGOwned <maksim.gojdin@gmail.com>
2024-12-07 22:05:17 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
-using namespace std;
-
-int main()
-{
-	cout << "Hello, 1 FIIT and 1 MO!" << endl;
-	return 0;
+using namespace std; 
+ 
+int main() { 
+    short p; 
+    cin >> p;
+    short * ptr = &p; 
+ 
+    unsigned char* x = reinterpret_cast<unsigned char*>(ptr); 
+    
+    cout << static_cast<short>(*x) << endl;
+    cout << static_cast<short>(*(x+1)) << endl;
 }
+ 
