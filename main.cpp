807aa5919eb106ddbce8e0bff7d5fc9cc159c6f5
Maxim Goydin <MLGoydin@stud.kantiana.ru>
2024-12-09 17:19:58 +0200

    Additional tasks

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 int main() { 
     short p; 
     cin >> p;
-    short * ptr = &p; 
+
+    short* ptr = &p; 
  
-    unsigned char* x = reinterpret_cast<unsigned char*>(ptr); 
+    unsigned char* x = reinterpret_cast<unsigned char*>(ptr); //  Первый байт переменной p
     
-    cout << static_cast<short>(*x) << endl;
-    cout << static_cast<short>(*(x+1)) << endl;
+    cout << static_cast<short>(*x) << endl; // Разыменовываем
+
+    cout << static_cast<short>(*(x+1)) << endl; // Смещаем на байт и разыменовываем второй байт
+
+	// cout << static_cast<short>(*(ptr + 2)) << endl;
 }
- 
